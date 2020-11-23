93e93a57c8f40f253f5a16768aef657f9e1354a2
ronaldKamphuis <info@ronaldkamphuis.com>
2020-11-23 13:26:03 +0100

    Begin gemaakt met security

---
+++ src/main/java/com/ronaldkamphuis/transcripties/ronald/oefenOpdracht/configuration/LibrarySecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.ronaldkamphuis.transcripties.ronald.oefenOpdracht.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author Ronald Kamphuis <info@ronaldkamphuis.com>
+ * Opdracht:
+ * Doel:
+ */
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class LibrarySecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").and()
+                .withUser("user").password(passwordEncoder().encode("user")).roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
